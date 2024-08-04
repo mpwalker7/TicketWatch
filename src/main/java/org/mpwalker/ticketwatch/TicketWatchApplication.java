a4a6797bba05672159ac02dc7427caa2fd8bad67
Vladislav Tyulyusin <karabasvlad@gmail.com>
2024-08-04 16:21:09 +0300

    Initial commit

---
+++ src/main/java/org/mpwalker/ticketwatch/TicketWatchApplication.java
@@ -0,0 +1,13 @@
+package org.mpwalker.ticketwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mpwalker/ticketwatch/TicketWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mpwalker.ticketwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
